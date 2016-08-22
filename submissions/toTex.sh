a225b492213781fa37a9bf7da92063f9385e0507
Aaron Lun <alun@wehi.edu.au>
2016-08-22 17:52:09 +0100

    Fixes to submission code.

--- submissions/toTex.sh
+++ submissions/toTex.sh
@@ -1,10 +1,11 @@
+set -e
+set -u
+
 cd ..
 pandoc -t latex workflow.md --bibliography ref.bib --biblatex --no-wrap \
     --template ~/Software/R/current/library/rmarkdown/rmd/latex/default.tex \
     --highlight-style tango > submissions/temp.tex
 cd - 
-
-cp ../figure/* .
 
 cat temp.tex | sed  "s/caption..textbf{Figure [^:]*:} /caption{/" > temp2.tex
 mv temp2.tex temp.tex
@@ -20,6 +21,19 @@ do
     cat temp.tex | sed "s/includegraphics{$x/includegraphics[width=\\\\maxwidth]{$x/" > temp2.tex
     mv temp2.tex temp.tex
 done
+
+cat temp.tex | grep "includegraphics" | grep ".png" | sed -E "s/.*\{([^\}]*)\}/\1/" > figures.txt
+mapfile -t allfigs < figures.txt
+for i in "${!allfigs[@]}"
+do
+    z=$((i+1))
+    newname=Figure${z}.png
+    cp ../figure/${allfigs[$i]} $newname
+    cat temp.tex | sed "s/${allfigs[$i]}/${newname}/" > temp2.tex
+    mv temp2.tex temp.tex
+done
+rm figures.txt
+
 
 cat preamble.tex temp.tex > workflow.tex
 rm temp.tex
