eb8f53e7c87fd4f398f644c857eed735440d39a7
Aaron Lun <alun@wehi.edu.au>
2017-09-19 08:10:55 +0100

    Fix to path for Bioc version.

--- toBioc.sh
+++ toBioc.sh
@@ -1,2 +1,2 @@
-cat workflow.Rmd | sed "s/on.bioc <- FALSE/on.bioc <- TRUE/" > ../bioc/workflow.Rmd
-cp ref.bib ../bioc
+cat workflow.Rmd | sed "s/on.bioc <- FALSE/on.bioc <- TRUE/" > ../bioc/vignettes/workflow.Rmd
+cp ref.bib ../bioc/vignettes
